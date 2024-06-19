e24a99ce1fe1366cba880811e3a88d895d006de5
crabcatcher0 <rautprashant103@gmail.com>
2024-06-19 10:21:48 +0545

    output as .txt file

--- OOP/BankAc.py
+++ OOP/BankAc.py
@@ -4,14 +4,23 @@ class BankAccount:
         self.balance = balance
 
     def deposite(self, amount):
-        total = amount + self.balance
-        return f"Balance Added. Current balance is {total}"
+        total = self.balance + amount 
+
+        with open("deposite.txt", "w") as file:
+            file.write(f"{amount}$ is deposited. Total balance is {total}$.")
+        return f"Please check the recipt."
+            
     
     def withdraw(self, amount):
         if amount > self.balance:
             print("Insufficent balance.")
-        reduced = amount - self.balance
-        return reduced
+        else:
+          reduced = self.balance - amount
+
+          with open("withdraw.txt", "w") as file:
+            file.write(f"{amount}$ has been withdraw. \nTotal balance is {reduced}$.")
+          
+        return f"Please check the recipt."
     
 r = BankAccount("Ram", 200)
-print(r.withdraw(400))
+print(r.withdraw(50))
